d2c35eaa7eb4cdcfc770f69fa234a898f25b30fd
FanHuaRan <834171100@qq.com>
2018-04-08 00:11:36 +0800

    c

---
+++ c/posixthread/posixhello/cmake-build-debug/CMakeFiles/posixhello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/posixhello.dir/thread1.c.o"
+  "CMakeFiles/posixhello.dir/thread2.c.o"
+  "posixhello.pdb"
+  "posixhello.exe"
+  "libposixhello.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/posixhello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c/posixthread/posixlock/cmake-build-debug/CMakeFiles/posixlock.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/posixlock.dir/main.c.o"
+  "posixlock.pdb"
+  "posixlock.exe"
+  "libposixlock.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/posixlock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

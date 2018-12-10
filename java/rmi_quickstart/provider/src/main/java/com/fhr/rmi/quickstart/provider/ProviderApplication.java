374c6240750f38cd592a182f983ec33dedc00469
FanHuaRan <834171100@qq.com>
2018-12-11 00:35:28 +0800

    update

--- java/rmi_quickstart/provider/src/main/java/com/fhr/rmi/quickstart/provider/ProviderApplication.java
+++ java/rmi_quickstart/provider/src/main/java/com/fhr/rmi/quickstart/provider/ProviderApplication.java
@@ -15,12 +15,13 @@ import java.rmi.server.UnicastRemoteObject;
 public class ProviderApplication {
 
     public static void main(String[] args) throws RemoteException {
+        // 实例化一个服务对象
         GreetingsService obj = new GreetingsServiceImpl(); // #1
+        // 导出远程服务对象，在29190端口进行监听，这个stud是个存根，基于jdk动态代理实现
         GreetingsService stub = (GreetingsService) UnicastRemoteObject.exportObject(obj, 29190); // #2
-
+        // 创建注册中心，监听1299端口，不过这个注册中心无法在多个应用间重用
         Registry registry = LocateRegistry.createRegistry(1299); //  默认注册端口1099
-
+        // 将远程服务在注册中心上进行注册
         registry.rebind("Greeting", stub); // #4
     }
-
 }

f4dabcd597864d415a763e034180318ee4e77368
huaran <huaran.fan@dmall.com>
2019-04-20 17:08:58 +0800

    update

---
+++ java/memcached/src/main/java/com/github/fhr/quickstart/memcached/Main.java
@@ -0,0 +1,47 @@
+package com.github.fhr.quickstart.memcached;
+
+import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.exception.MemcachedException;
+import net.rubyeye.xmemcached.utils.AddrUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author Fan Huaran
+ * created on 2019/4/20
+ * @description
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses("localhost:11211"));
+        MemcachedClient memcachedClient = builder.build();
+        try {
+            memcachedClient.set("hello", 0, "Hello,xmemcached");
+            String value = memcachedClient.get("hello");
+            System.out.println("hello=" + value);
+            memcachedClient.delete("hello");
+            value = memcachedClient.get("hello");
+            System.out.println("hello=" + value);
+        } catch (MemcachedException e) {
+            System.err.println("MemcachedClient operation fail");
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            System.err.println("MemcachedClient operation timeout");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        try {
+            //close memcached client
+            memcachedClient.shutdown();
+        } catch (IOException e) {
+            System.err.println("Shutdown MemcachedClient fail");
+            e.printStackTrace();
+        }
+    }
+
+}

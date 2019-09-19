56b4219e52f29aa963eb31885261548966a64ea6
huaran <huaran.fan@dmall.com>
2019-09-19 15:32:23 +0800

    update

---
+++ java/kafka/kafka-consumer/src/main/java/com/fhr/quickstart/kafka/SimpleConsumer.java
@@ -0,0 +1,48 @@
+package com.fhr.quickstart.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author Fan Huaran
+ * created on 2019/9/19
+ * @description
+ */
+public class SimpleConsumer {
+
+    private static final String TOPIC = "helloworld";
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "127.0.0.1:9092");
+        //        用来唯一标识consumer进程所在组的字符串，如果设置同样的group id，表示这些processes都是属于同一个consumer group
+        props.put("group.id", "group1");
+        //        如果为真，consumer所fetch的消息的offset将会自动的同步到zookeeper。这项提交的offset将在进程挂掉时，由新的consumer使用
+        props.put("enable.auto.commit", "true");
+        //        consumer向zookeeper提交offset的频率
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+        //  订阅topic，可以为多个用,隔开Arrays.asList("topic1","topic2");
+        consumer.subscribe(Arrays.asList(TOPIC));
+
+        while (true) {
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(100);
+
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord.value());
+            }
+
+        }
+    }
+
+}
